b9e8f5a6fecde6b51d10e25cd6452f21f9310c32
jariwalahetal <hetal.h.jariwala@gmail.com>
2018-11-13 17:01:31 -0500

    Created vs project

---
+++ SOEN7481.Utility/SOEN7481.Utility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOEN7481.Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
