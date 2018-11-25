d7b1d6fd659ef9cbd9e577f2f55834d634059bfb
Hetal Jariwala <hetal.h.jariwala@gmail.com>
2018-11-25 17:47:23 -0500

    W.I.P data collection

--- SOEN7481.Utility/SOEN7481.Utility/Client.cs
+++ SOEN7481.Utility/SOEN7481.Utility/Client.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SOEN7481.Utility
@@ -15,6 +16,19 @@ namespace SOEN7481.Utility
         private String RepositoryName { get; set; }
         private String OwnerName { get; set; }
 
+        List<PullRequestResponse> allRequests = new List<PullRequestResponse>();
+        List<CommitResponse> allCommits = new List<CommitResponse>();
+
+        List<string> requestMergedBy = new List<string>();
+        private List<string> coreDevelopers;
+
+        static List<string> tokens = new List<string> {
+            "7b84ebec3dd096e3c24a5277bc0c01229bff31a2",
+        };
+
+        static int tokenId = 0;
+
+        public Boolean breakOperation = false;
         public Client(string ownerName, string repositoryName)
         {
             RepositoryName = repositoryName;
@@ -23,21 +37,23 @@ namespace SOEN7481.Utility
 
         public async Task<int> GetCommitsCountsWithErrorAsync()
         {
-
-            List<CommitResponse> allCommits = new List<CommitResponse>();
-
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Accept", " application/vnd.github.inertia-preview+json");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "408c951acfa303f4a6e0a8b0118dba9aab616f74");
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("jariwalahetal");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens[tokenId]);
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("shriyans16");
             string queryString = "";
             while (true)
             {
                 String path = String.Format("repos/{0}/{1}/commits{2}", OwnerName, RepositoryName, queryString);
 
                 HttpResponseMessage response = await client.GetAsync(path);
+                if (!validateRateLimit(response))
+                {
+                    breakOperation = true;
+                    return -1;
+                }
                 if (response.IsSuccessStatusCode)
                 {
                     string s = await response.Content.ReadAsStringAsync();
@@ -73,24 +89,31 @@ namespace SOEN7481.Utility
         {
             Program.PrintInfo("Getting admin members");
             List<string> adminMembers = await GetAdminMembersAsync();
+            if (breakOperation)
+                return -1;
+
             Program.PrintInfo("Getting pull request accepted members");
             List<string> pullRequestsAcceptedMembers = await GetPullRequestAcceptedLoginIdAsync();
+            if (breakOperation)
+                return -1;
+
             Program.PrintInfo("Getting issues closed members");
             List<string> issuesClosedMembers = await GetIssuesClosedLoginIdAsync();
+            if (breakOperation)
+                return -1;
 
-            var finalList = adminMembers.Union(pullRequestsAcceptedMembers).Union(issuesClosedMembers).Distinct();
-            return finalList.Count();
+            coreDevelopers = adminMembers.Union(pullRequestsAcceptedMembers).Union(issuesClosedMembers).Distinct().ToList();
+         
+            return coreDevelopers.Count();
         }
 
         private async Task<List<string>> GetAdminMembersAsync()
         {
-            List<CommitResponse> allCommits = new List<CommitResponse>();
-
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Accept", " application/vnd.github.inertia-preview+json");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "408c951acfa303f4a6e0a8b0118dba9aab616f74");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens[tokenId]);
             client.DefaultRequestHeaders.UserAgent.ParseAdd("jariwalahetal");
             string queryString = "";
             while (true)
@@ -98,6 +121,11 @@ namespace SOEN7481.Utility
                 String path = String.Format("orgs/{0}/members?role=admin", OwnerName);
 
                 HttpResponseMessage response = await client.GetAsync(path);
+                if (!validateRateLimit(response))
+                {
+                    breakOperation = true;
+                    return null;
+                }
                 if (response.IsSuccessStatusCode)
                 {
                     string s = await response.Content.ReadAsStringAsync();
@@ -106,19 +134,17 @@ namespace SOEN7481.Utility
                 }
 
             }
-            return new List<string>();
         }
 
         private async Task<List<string>> GetPullRequestAcceptedLoginIdAsync()
         {
             List<string> coreDevelopersIds = new List<string>();
-            List<PullRequestResponse> allRequests = new List<PullRequestResponse>();
 
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Accept", " application/vnd.github.inertia-preview+json");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "408c951acfa303f4a6e0a8b0118dba9aab616f74");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens[tokenId]);
             client.DefaultRequestHeaders.UserAgent.ParseAdd("jariwalahetal");
             string queryString = "";
             int pageno = 1;
@@ -127,6 +153,11 @@ namespace SOEN7481.Utility
                 String path = String.Format("repos/{0}/{1}/pulls?state=closed&page={2}", OwnerName, RepositoryName, pageno++);
 
                 HttpResponseMessage response = await client.GetAsync(path);
+                if (!validateRateLimit(response))
+                {
+                    breakOperation = true;
+                    return null;
+                }
                 if (response.IsSuccessStatusCode)
                 {
                     string s = await response.Content.ReadAsStringAsync();
@@ -141,22 +172,25 @@ namespace SOEN7481.Utility
                 break;
             }
 
+            Program.PrintInfo("Processing pull requests..");
+            //If merged_at is null then PR is rejected
+            int index = 0;
             foreach ( var request in allRequests)
             {
-                String path = String.Format("repos/{0}/{1}/commits/{2}", OwnerName, RepositoryName, request.merge_commit_sha);
-
-                HttpResponseMessage response = await client.GetAsync(path);
-                if (response.IsSuccessStatusCode)
+                var commit = allCommits.FirstOrDefault(c => c.sha == request.merge_commit_sha);
+                if (commit != null)
                 {
-                    string s = await response.Content.ReadAsStringAsync();
-                    var commitRequest = JsonConvert.DeserializeObject<CommitResponse>(s);
-                    if (commitRequest?.sha == request?.merge_commit_sha)
+                    requestMergedBy.Add(commit?.author?.id.ToString());
+                    if (commit?.sha == request?.merge_commit_sha)
                     {
-                        if (commitRequest?.author?.id != commitRequest?.committer?.id)
+                        if (commit?.author?.id != commit?.committer?.id)
                         {
-                            coreDevelopersIds.Add(commitRequest.committer.id.ToString());
+                            coreDevelopersIds.Add(commit.committer.id.ToString());
                         }
                     }
+                }
+                else {
+                    requestMergedBy.Add("");
                 }
             }
 
@@ -173,7 +207,7 @@ namespace SOEN7481.Utility
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Accept", " application/vnd.github.inertia-preview+json");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "408c951acfa303f4a6e0a8b0118dba9aab616f74");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens[tokenId]);
             client.DefaultRequestHeaders.UserAgent.ParseAdd("jariwalahetal");
 
             List<string> milestones = await GetMilestones();
@@ -186,14 +220,21 @@ namespace SOEN7481.Utility
                     String path = String.Format("repos/{0}/{1}/issues?state=closed&page={2}&milestone={3}", OwnerName, RepositoryName, pageno++, milestone);
 
                     HttpResponseMessage response = await client.GetAsync(path);
-                    if (response.IsSuccessStatusCode)
+                    if (!validateRateLimit(response))
                     {
-                        string s = await response.Content.ReadAsStringAsync();
-                        var closedIssues = JsonConvert.DeserializeObject<List<IssuesResponse>>(s);
-                        if (closedIssues != null && closedIssues.Count > 0)
+                        breakOperation = true;
+                    }
+                    else
+                    {
+                        if (response.IsSuccessStatusCode)
                         {
-                            allClosedIssues.AddRange(closedIssues);
-                            continue;
+                            string s = await response.Content.ReadAsStringAsync();
+                            var closedIssues = JsonConvert.DeserializeObject<List<IssuesResponse>>(s);
+                            if (closedIssues != null && closedIssues.Count > 0)
+                            {
+                                allClosedIssues.AddRange(closedIssues);
+                                continue;
+                            }
                         }
                     }
                     break;
@@ -205,6 +246,11 @@ namespace SOEN7481.Utility
                 String path = String.Format("repos/{0}/{1}/issues/{2}", OwnerName, RepositoryName, request.number);
 
                 HttpResponseMessage response = await client.GetAsync(path);
+                if (!validateRateLimit(response))
+                {
+                    breakOperation = true;
+                    return null;
+                }
                 if (response.IsSuccessStatusCode)
                 {
                     string s = await response.Content.ReadAsStringAsync();
@@ -226,7 +272,7 @@ namespace SOEN7481.Utility
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("Accept", " application/vnd.github.inertia-preview+json");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "408c951acfa303f4a6e0a8b0118dba9aab616f74");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens[tokenId]);
             client.DefaultRequestHeaders.UserAgent.ParseAdd("jariwalahetal");
             string queryString = "";
             int pageno = 1;
@@ -235,6 +281,11 @@ namespace SOEN7481.Utility
                 String path = String.Format("repos/{0}/{1}/milestones?state=all&page={2}", OwnerName, RepositoryName, pageno++);
 
                 HttpResponseMessage response = await client.GetAsync(path);
+                if (!validateRateLimit(response))
+                {
+                    breakOperation = true;
+                    break;
+                }
                 if (response.IsSuccessStatusCode)
                 {
                     string s = await response.Content.ReadAsStringAsync();
@@ -250,5 +301,92 @@ namespace SOEN7481.Utility
             milestoneNumbers.Add("none");
             return milestoneNumbers;
         }
+
+        public int GetTotalPullRequestCount() {
+            return allRequests.Count();
+        }
+
+        public int GetTotalMergedPullRequestCount()
+        {
+            return allRequests.Where(x => !String.IsNullOrEmpty(x.merged_at)).Count();
+        }
+
+        public int GetTotalRejectedPullRequestCount()
+        {
+            return allRequests.Where(x => String.IsNullOrEmpty(x.merged_at)).Count();
+        }
+
+        public int GetTotalMergedRequestByCoreDevelopersCount()
+        {
+            int count = 0;
+            for (int i = 0; i < allRequests.Count(); i++) {
+                if (!String.IsNullOrEmpty(allRequests[i].merged_at) && coreDevelopers.Contains(requestMergedBy[i])) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetTotalRejectedRequestByCoreDevelopersCount()
+        {
+            int count = 0;
+            for (int i = 0; i < allRequests.Count(); i++)
+            {
+                if (String.IsNullOrEmpty(allRequests[i].merged_at) && coreDevelopers.Contains(requestMergedBy[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetTotalMergedRequestByNonCoreDevelopersCount()
+        {
+            int count = 0;
+            for (int i = 0; i < allRequests.Count(); i++)
+            {
+                if (!String.IsNullOrEmpty(allRequests[i].merged_at) && !coreDevelopers.Contains(requestMergedBy[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetTotalRejectedRequestByNonCoreDevelopersCount()
+        {
+            int count = 0;
+            for (int i = 0; i < allRequests.Count(); i++)
+            {
+                if (String.IsNullOrEmpty(allRequests[i].merged_at) && !coreDevelopers.Contains(requestMergedBy[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private bool validateRateLimit(HttpResponseMessage response)
+        {
+            string headers = response.Headers.ToString();
+
+            int pendingRequests = Convert.ToInt32(headers.Substring(headers.IndexOf("X-RateLimit-Remaining: ") + 23).Substring(0, headers.Substring(headers.IndexOf("X-RateLimit-Remaining: ") + 23).IndexOf("\r\n")));
+
+            double limitRest = Convert.ToDouble(headers.Substring(headers.IndexOf("X-RateLimit-Reset: ") + 19).Substring(0, headers.Substring(headers.IndexOf("X-RateLimit-Reset: ") + 19).IndexOf("\r\n")));
+
+            DateTime resumeTime = FromUnixTime(limitRest);
+            if (pendingRequests < 5)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private DateTime FromUnixTime(double limitRest)
+        {
+            return epoch.AddSeconds(limitRest);
+        }
+        
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     }
 }
--- SOEN7481.Utility/SOEN7481.Utility/Program.cs
+++ SOEN7481.Utility/SOEN7481.Utility/Program.cs
@@ -16,6 +16,12 @@ namespace SOEN7481.Utility
         static List<string> ownerlogin = new List<string>();
         static List<int> numberOfCoreBugs = new List<int>();
         static List<int> coreDevelopersCounts = new List<int>();
+        static List<int> mergedRequestCounts = new List<int>();
+        static List<int> rejectedRequestsCounts = new List<int>();
+        static List<int> mergedRequestByCoreDevelopersCount = new List<int>();
+        static List<int> rejectedRequestByCoreDevelopersCount = new List<int>();
+        static List<int> mergedRequestByNonCoreDevelopersCount = new List<int>();
+        static List<int> rejectedRequestByNonCoreDevelopersCount = new List<int>();
 
         static void Main(string[] args)
         {
@@ -57,12 +63,12 @@ namespace SOEN7481.Utility
 
         private static async System.Threading.Tasks.Task getGitHubInfoAsync()
         {
-
+            int counter = 0;
             try
             {
                 PrintInfo("Gettng data from github...");
                 //Get information from Github
-                for (int i = 0; i < repoName.Count; i++)
+                for (int i = counter; i < repoName.Count; i++)
                 {
                     PrintInfo("Current reponame - " + repoName[i]);
 
@@ -70,33 +76,62 @@ namespace SOEN7481.Utility
 
                     PrintInfo("Getting core bugs");
                     int coreBugs = await c.GetCommitsCountsWithErrorAsync();
+
+                    if (c.breakOperation)
+                    {
+                        break;
+                    }
                     numberOfCoreBugs.Add(coreBugs);
                     PrintInfo(repoName[i] + " - Number of bugs:" + coreBugs);
 
                     PrintInfo("Getting core developers");
                     int count = await c.GetCoreDeveloerCountAsync();
+                    if (c.breakOperation)
+                    {
+                        break;
+                    }
                     coreDevelopersCounts.Add(count);
                     PrintInfo(repoName[i] + " - Core developers count:" + count);
-                }
 
-                printResult();
+                    mergedRequestCounts.Add(c.GetTotalMergedPullRequestCount());
+                    rejectedRequestsCounts.Add(c.GetTotalRejectedPullRequestCount());
+
+                    mergedRequestByCoreDevelopersCount.Add(c.GetTotalMergedRequestByCoreDevelopersCount());
+                    rejectedRequestByCoreDevelopersCount.Add(c.GetTotalRejectedRequestByCoreDevelopersCount());
+
+                    mergedRequestByNonCoreDevelopersCount.Add(c.GetTotalMergedRequestByNonCoreDevelopersCount());
+                    rejectedRequestByNonCoreDevelopersCount.Add(c.GetTotalRejectedRequestByNonCoreDevelopersCount());
+                    counter++;
+                }
             }
             catch (Exception ex)
             {
                 PrintError(ex.Message);
             }
+
+            printResult(counter);
         }
 
-        private static void printResult()
+        private static void printResult(int count)
         {
             PrintInfo("Generating result...");
 
             var csv = new StringBuilder();
-            csv.AppendLine("repo_name,owner_login,core_bugs,test");
+            csv.AppendLine("repo_name,owner_login,core_bugs,core_developer,total_merged_pr,total_rejected_pr,merged_pr_by_core_developer,rejected_pr_by_core_developers,merged_pr_by_ext_developer,rejected_pr_by_ext_developer");
 
-            for (int i = 0; i < repoName.Count; i++)
+            for (int i = 0; i < rejectedRequestByNonCoreDevelopersCount.Count; i++)
             {
-                var newLine = string.Format("{0},{1},{2},{3}", repoName[i], ownerlogin[i], numberOfCoreBugs[i], coreDevelopersCounts[i]);
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", 
+                            repoName[i], 
+                            ownerlogin[i], 
+                            numberOfCoreBugs[i], 
+                            coreDevelopersCounts[i],
+                            mergedRequestCounts[i],
+                            rejectedRequestsCounts[i],
+                            mergedRequestByCoreDevelopersCount[i],
+                            rejectedRequestByCoreDevelopersCount[i],
+                            mergedRequestByNonCoreDevelopersCount[i],
+                            rejectedRequestByNonCoreDevelopersCount[i]);
                 csv.AppendLine(newLine);
             }
 
