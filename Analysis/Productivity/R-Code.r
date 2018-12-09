ced1140e449d9651b17289a9a3461d793fd0b8b9
Hetal Jariwala <hetal.h.jariwala@gmail.com>
2018-12-09 16:33:11 -0500

    Added images for analysis in R and R Code

---
+++ Analysis/Productivity/R-Code.r
@@ -0,0 +1,40 @@
+# install.packages("MASS")
+library(MASS)
+# install.packages("pscl")
+library(pscl)
+# insall.packages("AER")
+library(AER)
+
+mydata<- read.csv("C:\\Users\\win10\\Desktop\\productivity.csv")
+attach(mydata)
+
+# Define variables
+Y1 <- cbind(merged_pr_by_core_developer)
+Y2 <- cbind(rejected_pr_by_core_developers)
+Y3 <- cbind(merged_pr_by_ext_developer)
+Y4 <- cbind(rejected_pr_by_ext_developer)
+
+X1 <- cbind(ï..team_size, proj_age, computed_n_stars, computed_n_forks, computed_n_src_loc, ci_integration)
+X2 <- cbind(ï..team_size, proj_age, computed_n_stars, computed_n_forks, computed_n_src_loc, ci_integration)
+
+# Descriptive statistics
+summary(Y1)
+summary(Y2)
+summary(Y3)
+summary(Y4)
+summary(X1)
+summary(X2)
+
+# Poisson model coefficients
+poissonmodel <- glm(Y1 ~ X1, family = poisson)
+summary(poissonmodel)
+
+# Test for overdispersion (dispersion and alpha parameters) from AER package
+dispersiontest(poissonmodel)
+
+# Zero-inflated negative binomial model coefficients
+zinb <- zeroinfl(Y1 ~ X1 | X2, link = "logit", dist = "poisson")
+summary(zinb)
+
+#vuong test
+vuong(zinb, poissonmodel)
---
+++ Analysis/Quality/R-Code.r
@@ -0,0 +1,36 @@
+# install.packages("MASS")
+library(MASS)
+# install.packages("pscl")
+library(pscl)
+# insall.packages("AER")
+library(AER)
+
+mydata<- read.csv("C:\\Users\\win10\\Desktop\\quality.csv")
+attach(mydata)
+
+# Define variables
+Y1 <- cbind(n_core_bugs)
+Y2 <- cbind(n_user_bugs)
+
+X1 <- cbind(n_non_bug_issues, proj_age, computed_n_stars, computed_n_forks, computed_n_src_loc, ci_integration)
+X2 <- cbind(n_non_bug_issues, proj_age, computed_n_stars, computed_n_forks, computed_n_src_loc, ci_integration)
+
+# Descriptive statistics
+summary(Y1)
+summary(Y2)
+summary(X1)
+summary(X2)
+
+# Poisson model coefficients
+poissonmodel <- glm(Y1 ~ X1, family = poisson)
+summary(poissonmodel)
+
+# Test for overdispersion (dispersion and alpha parameters) from AER package
+dispersiontest(poissonmodel)
+
+# Zero-inflated negative binomial model coefficients
+zinb <- zeroinfl(Y1 ~ X1, link = "logit", dist = "poisson")
+summary(zinb)
+
+#vuong test
+vuong(zinb, poissonmodel)
